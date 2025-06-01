9b5633e0d91cb45e42dcb45ada39742be698356f
ashwom2017 <aidanshwom@gmail.com>
2025-06-01 14:30:03 -0500

    Work in progress: Home section updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', redirectTo: '' }
+];
