768e46eec158532dd534838b3207a30300937c7a
ashwom2017 <aidanshwom@gmail.com>
2025-09-16 21:47:41 -0500

    scrolling changes

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,26 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideZoneChangeDetection } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
-
-import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { routes } from './app.routes';
+import { provideZoneChangeDetection } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(),
     provideAnimations(),
     importProvidersFrom(
-      RouterModule.forRoot(routes),
+      RouterModule.forRoot(routes, {
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      }),
       MatCardModule,
       MatButtonModule,
       FormsModule
